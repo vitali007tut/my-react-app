bc5e2fa1d861c4af3fcef80e80022ec2aa029fb0
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-07-29 09:29:43 +0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://vitali007tut.github.io/',
+    base: 'https://vitali007tut.github.io/my-react-app/',
     plugins: [react()],
 });
